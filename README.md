19b066220514d74ef09442e967f669c6e1327308
raphaeljoie <9681129+raphaeljoie@users.noreply.github.com>
2021-07-12 20:37:35 +0200

    🖊 illustrate README introduction

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Lowlight React component
 [![NPM](https://img.shields.io/npm/v/lowlight-react.svg)](https://www.npmjs.com/package/lowlight-react) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
+> Include code snippets like a pro using a light ReactJS wrapper
+> around [Lowlight](https://github.com/wooorm/lowlight)
+
+![./example/](./example/doc/screenshot.png)
+
+- [x] **code highlight using [Highlight.js](https://highlightjs.org/)**, including 196 languages and 242 styles
+- [x] **line numbering and wrapping** fully css-styleable
+- [x] **Optimized experience** both for devs and end users
+
 ## Install
 
 ```bash
--- example/src/App.js
+++ example/src/App.js
@@ -10,13 +10,13 @@ const text = 'Blabetiblou World!'
 console.log(text)`
 
   return (
-    <div style={{background: 'white'}}>
+    <div style={{padding: 30}}>
       <h1>Code as property</h1>
       <Lowlight language="javascript" value={code} />
       <h1>Code as children</h1>
       <Lowlight language="javascript">
         {`
-const text = 'Blabetiblou World!'
+const text = 'Blabetiblou World! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nibh sapien, imperdiet a est eu, consequat dictum mauris. Etiam porttitor justo ac lorem ultrices tincidunt. In eu est dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam bibendum est in lorem varius euismod.'
 console.log(text)
 
 // don't execute !
